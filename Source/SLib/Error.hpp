10fbbcfa8b070e1123cb96ab1bc2a0c2cc8e60a7
Su <z69500jqk@qq.com>
2025-06-28 00:22:55 +0800

    初步设定项目结构

---
+++ Source/SLib/Error.hpp
@@ -0,0 +1,8 @@
+﻿/**
+ * @File Error.hpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2025/6/28
+ * @Brief This file is part of SLib.
+ */
+ 
+#pragma once
---
+++ Source/SLib/SLib.cpp
@@ -0,0 +1,6 @@
+﻿/**
+ * @File SLib.cpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2025/6/28
+ * @Brief This file is part of SLib.
+ */
