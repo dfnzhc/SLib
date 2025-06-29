2cbdb635388b8c495d4385026b76e0031901e538
Su <z69500jqk@qq.com>
2025-06-29 15:30:33 +0800

    两个工具类型

---
+++ Source/SLib/Utility/Utility.hpp
@@ -0,0 +1,53 @@
+﻿/**
+ * @File Utility.hpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2025/6/29
+ * @Brief This file is part of SLib.
+ */
+
+#pragma once
+
+#include <optional>
+#include <expected>
+
+#include <SLib/String/StringType.hpp>
+#include <SLib/Enum/Enum.hpp>
+
+namespace slib {
+
+// Optional
+template<typename T>
+using Opt = std::optional<T>;
+
+// Result
+enum class FailureType
+{
+    Failed = 0, ///> General Failed.
+};
+
+constexpr std::string_view FailureName(FailureType failure)
+{
+    return me::enum_name(failure);
+}
+
+using UnexpectedType = std::pair<FailureType, String>;
+
+inline std::unexpected<UnexpectedType> Unexpected(FailureType failure, StringView msg)
+{
+    return std::unexpected<UnexpectedType>{
+      {failure, String(msg)}
+    };
+}
+
+template<typename... Args>
+std::unexpected<UnexpectedType> Unexpected(FailureType failure, std::format_string<Args...> fmt, Args&&... args)
+{
+    return std::unexpected<UnexpectedType>{
+      {failure, String(std::format(fmt, std::forward<Args>(args)...))}
+    };
+}
+
+template<typename T>
+using Result = std::expected<T, UnexpectedType>;
+
+} // namespace slib
