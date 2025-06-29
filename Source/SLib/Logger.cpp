09bf42087b2910093e97cbc0b0da1abe20e878d8
Su <z69500jqk@qq.com>
2025-06-29 14:46:17 +0800

    简单的日志实现

---
+++ Source/SLib/Logger.cpp
@@ -0,0 +1,74 @@
+﻿/**
+ * @File Logger.cpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2025/6/29
+ * @Brief This file is part of SLib.
+ */
+
+#include "Logger.hpp"
+#include "Error.hpp"
+
+#include <mutex>
+#include <fmt/color.h>
+
+using namespace slib;
+
+namespace {
+constexpr inline const char* logLevelString(Logger::Level level)
+{
+    switch (level) {
+    case Logger::Level::Fatal   : return "Fatal";
+    case Logger::Level::Error   : return "Error";
+    case Logger::Level::Warning : return " Warn";
+    case Logger::Level::Info    : return " Info";
+    case Logger::Level::Trace   : return "Trace";
+    }
+    SLIB_UNREACHABLE();
+}
+
+std::mutex sMutex = {};
+Logger::Level sCurrentLevel =
+#ifdef SLIB_ENABLE_DEBUG
+    Logger::Level::Trace;
+#else
+    Logger::Level::Info;
+#endif
+} // namespace
+
+Logger::Logger() { }
+
+Logger::~Logger() { }
+
+void Logger::log(Level level, std::string_view msg, u32 indent)
+{
+    auto lock = std::lock_guard(sMutex);
+
+    if (level > sCurrentLevel)
+        return;
+
+    fmt::color color = fmt::color::white;
+    switch (level) {
+    case Fatal   : color = fmt::color::red; break;
+    case Error   : color = fmt::color::magenta; break;
+    case Warning : color = fmt::color::coral; break;
+    case Info    : color = fmt::color::green_yellow; break;
+    case Trace   : color = fmt::color::white_smoke; break;
+    }
+
+    // TODO: [%(time)][%(thread_id)][%(short_source_location)][%(log_level)]%(tags): %(message)
+    const auto s = fmt::format(fmt::fg(color), "{}[{}]: {}", std::string(indent * 2, ' '), logLevelString(level), msg);
+    auto& os     = std::cout;
+    os << s << '\n';
+    std::flush(os);
+}
+
+void Logger::setLevel(Level level)
+{
+    auto lock     = std::lock_guard(sMutex);
+    sCurrentLevel = level;
+}
+
+void detail::LogWithSourceLocation(Logger::Level level, std::source_location sl, std::string_view msg)
+{
+    Logger::Get().log(level, fmt::format("{}: '{}' {}({}:{})", msg, sl.function_name(), sl.file_name(), sl.line(), sl.column()));
+}
---
+++ Source/SLib/Logger.hpp
@@ -0,0 +1,129 @@
+﻿/**
+ * @File Logger.hpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2025/6/29
+ * @Brief This file is part of SLib.
+ */
+
+#pragma once
+
+#include <source_location>
+
+#include <SLib/String/StringType.hpp>
+#include <SLib/Math/Numeric.hpp>
+
+namespace slib {
+
+class Logger
+{
+public:
+    enum class Level
+    {
+        Fatal,
+        Error,
+        Warning,
+        Info,
+        Trace
+    };
+    using enum Level;
+
+    static Logger& Get()
+    {
+        static Logger logger;
+        return logger;
+    }
+
+    void log(Level level, std::string_view msg, u32 indent = 0);
+    void setLevel(Level level);
+
+private:
+    Logger();
+    ~Logger();
+};
+
+// @formatter:off
+
+inline void LogTrace(std::string_view msg)
+{
+    Logger::Get().log(Logger::Level::Trace, msg);
+}
+
+template<typename... Args>
+inline void LogTrace(fmt::format_string<Args...> format, Args&&... args)
+{
+    Logger::Get().log(Logger::Level::Trace, fmt::format(format, std::forward<Args>(args)...));
+}
+
+inline void LogTrace(u32 indent, std::string_view msg)
+{
+    Logger::Get().log(Logger::Level::Trace, msg, indent);
+}
+
+template<typename... Args>
+inline void LogTrace(u32 indent, fmt::format_string<Args...> format, Args&&... args)
+{
+    Logger::Get().log(Logger::Level::Trace, fmt::format(format, std::forward<Args>(args)...), indent);
+}
+
+inline void LogInfo(std::string_view msg)
+{
+    Logger::Get().log(Logger::Level::Info, msg);
+}
+
+template<typename... Args>
+inline void LogInfo(fmt::format_string<Args...> format, Args&&... args)
+{
+    Logger::Get().log(Logger::Level::Info, fmt::format(format, std::forward<Args>(args)...));
+}
+
+inline void LogInfo(u32 indent, std::string_view msg)
+{
+    Logger::Get().log(Logger::Level::Info, msg, indent);
+}
+
+template<typename... Args>
+inline void LogInfo(u32 indent, fmt::format_string<Args...> format, Args&&... args)
+{
+    Logger::Get().log(Logger::Level::Info, fmt::format(format, std::forward<Args>(args)...), indent);
+}
+
+inline void LogWarn(std::string_view msg)
+{
+    Logger::Get().log(Logger::Level::Warning, msg);
+}
+
+template<typename... Args>
+inline void LogWarn(fmt::format_string<Args...> format, Args&&... args)
+{
+    Logger::Get().log(Logger::Level::Warning, fmt::format(format, std::forward<Args>(args)...));
+}
+
+inline void LogWarn(u32 indent, std::string_view msg)
+{
+    Logger::Get().log(Logger::Level::Warning, msg, indent);
+}
+
+template<typename... Args>
+inline void LogWarn(u32 indent, fmt::format_string<Args...> format, Args&&... args)
+{
+    Logger::Get().log(Logger::Level::Warning, fmt::format(format, std::forward<Args>(args)...), indent);
+}
+
+namespace detail {
+
+void LogWithSourceLocation(Logger::Level level, std::source_location sl, std::string_view msg);
+
+template<typename... Args>
+inline void LogWithSourceLocation(Logger::Level level, std::source_location sl, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    LogWithSourceLocation(level, sl, fmt::format(fmt, std::forward<Args>(args)...));
+}
+
+} // namespace detail
+
+#define LogError(...) detail::LogWithSourceLocation(Logger::Level::Error, std::source_location::current(), __VA_ARGS__)
+
+#define LogFatal(...) detail::LogWithSourceLocation(Logger::Level::Fatal, std::source_location::current(), __VA_ARGS__)
+
+// @formatter:on
+} // namespace slib
